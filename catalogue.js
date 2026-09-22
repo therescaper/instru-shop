789f741c486f2c81fd56bbf0eae5ba0dc23950df
therescaper <therescaper2@gmail.com>
2026-09-22 10:12:10 +0200

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -8,7 +8,7 @@ window.SHOP = {
   orderMode: 'discord', // catalogue, ingame ou discord
   discord: 'therescaper',
   products: [
-    { id: 'armure', name: 'Armure complète en diamant', category: 'Armures', image: 'diamond_chestplate', images: ['diamond_helmet','diamond_chestplate','diamond_leggings','diamond_boots'], description: 'Casque, plastron, jambières et bottes.', unit: 'ensemble de 4 pièces', prices: { diamants: 16 } },
+    { id: 'armure', name: 'Armure complète en diamant', category: 'Armures', image: 'diamond_chestplate', images: ['diamond_helmet','diamond_chestplate','diamond_leggings','diamond_boots'], description: 'Casque, plastron, jambières et bottes.', unit: 'ensemble de 4 pièces', prices: { diamants: 12 } },
     { id: 'pioche', name: 'Pioche en diamant', category: 'Outils', image: 'diamond_pickaxe', description: 'Pour tes sessions de minage.', unit: 'pièce', prices: { fer: 8, or: 8 } },
     { id: 'epee', name: 'Épée en diamant', category: 'Armes', image: 'diamond_sword', description: 'Pour partir à l’aventure bien équipé.', unit: 'pièce', prices: { fer: 8, or: 8 } },
     { id: 'pelle', name: 'Pelle en diamant', category: 'Outils', image: 'diamond_shovel', description: 'Pour préparer le terrain de tes projets.', unit: 'pièce', prices: { fer: 8, or: 8 } },
