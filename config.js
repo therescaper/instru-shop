91d4ba9521bf955a3a4653b40a1bbc2ff4c00315
therescaper <therescaper2@gmail.com>
2026-09-26 09:46:56 +0200

    Activer l envoi Discord des commandes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-// Colle ici l’URL publique de ton Cloudflare Worker quand il sera créé.
+// URL publique du Cloudflare Worker qui relaie les commandes vers Discord.
 // Ne mets jamais l’URL du webhook Discord dans ce fichier public.
-window.ORDER_WEBHOOK_ENDPOINT = '';
+window.ORDER_WEBHOOK_ENDPOINT = 'https://instru-shop-orders.therescaper2.workers.dev/';
